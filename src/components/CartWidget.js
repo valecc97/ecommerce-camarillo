ad62182b6ad7a23377bf30b4da22ca4eeecfac28
valecam97 <valeriacamarillo97@gmail.com>
2022-08-02 21:03:16 -0500

    Componentes cart e itemlistcontainer

---
+++ src/components/CartWidget.js
@@ -0,0 +1,9 @@
+import cartIcon from '../images/cart.svg';
+
+function CartWidget() {
+	return (
+		<img src={cartIcon} className="nav-icon" alt="cart" />
+	)
+}
+
+export default CartWidget;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,17 @@
 import './NavBar.css';
+import CartWidget from './CartWidget.js';
 import pflogo from '../images/pf-logo.svg';
 
 function NavBar() {
 	return (
-		<div>
+		<div className="navBar-wrapper">
 			<ul className="navBar">
 				<li className="navBar-item"><a href="#"><img src={pflogo} className="store-logo" alt="logo" /></a></li>
 				<li className="navBar-item"><a href="#">Productos</a></li>
 				<li className="navBar-item"><a href="#">Pretty Kits</a></li>
 				<li className="navBar-item"><a href="#">Nosotros</a></li>
+				<li className="navBar-icon"><CartWidget/></li>
+				
 			</ul>
 		</div>
 	)
